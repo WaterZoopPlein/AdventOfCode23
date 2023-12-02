a5044d9071f435764e67c0c6423a5bc65c28fe9f
Nguyen MK <28999049+WaterZoopPlein@users.noreply.github.com>
2023-12-02 16:34:26 +0000

    Day 02 completed

---
+++ AdventOfCode23Day/Day02.cs
@@ -0,0 +1,87 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCode23Common;
+
+namespace AdventOfCode23Day;
+
+public class Day02 : IDay
+{
+    private List<string> _inputList = new();
+    private string _inputString = "";
+    private List<Dictionary<string, int>> _minimumCubesBagList = new();
+
+    public void Initialise()
+    {
+        _inputList = ReadInput.ConvertInputTextToStringLineList(@"..\..\..\..\Inputs\Day02.txt");
+        var regex = new Regex(@"(?<=Game\s\d*:\s).*");
+        var matches = regex.Matches(_inputString);
+        for (int i = 0; i < matches.Count; i++)
+        {
+            _inputList.Add(matches[i].Value);
+        }
+
+        _minimumCubesBagList = _inputList.Select(DeductMinimumCubes).ToList();
+    }
+
+    public void SolvePartOne()
+    {
+        var thresholdDict = new Dictionary<string, int>
+        {
+            { "blue", 14 },
+            { "red", 12 },
+            { "green", 13 }
+        };
+
+        int idSum = 0;
+        for (int i = 0; i < _minimumCubesBagList.Count; i++)
+        {
+            var isExceedingThreshold =
+                _minimumCubesBagList[i]["blue"] > thresholdDict["blue"] ||
+                _minimumCubesBagList[i]["red"] > thresholdDict["red"] ||
+                _minimumCubesBagList[i]["green"] > thresholdDict["green"];
+            if (!isExceedingThreshold)
+            {
+                idSum += i + 1;
+            }
+        }
+
+        Console.WriteLine(idSum);
+    }
+
+    public void SolvePartTwo()
+    {
+        long powerSum = 0;
+        for (int i = 0; i < _minimumCubesBagList.Count; i++)
+        {
+            powerSum +=
+                _minimumCubesBagList[i]["blue"]
+                * _minimumCubesBagList[i]["red"]
+                * _minimumCubesBagList[i]["green"];
+        }
+
+        Console.WriteLine(powerSum);
+    }
+
+    public Dictionary<string, int> DeductMinimumCubes(string gameString)
+    {
+        var regex = new Regex(@"(?<=Game\s\d*:\s).*");
+        var match = regex.Matches(gameString)[0].Value;
+
+        var outputDict = new Dictionary<string, int>
+        {
+            { "blue", 0 },
+            { "red", 0 },
+            { "green", 0 }
+        };
+
+        foreach (var hand in match.Split(',', ';'))
+        {
+            var splitHand = hand.Trim().Split(" ");
+            outputDict[splitHand[1]] =
+                outputDict[splitHand[1]] < int.Parse(splitHand[0])
+                    ? int.Parse(splitHand[0])
+                    : outputDict[splitHand[1]];
+        }
+
+        return outputDict;
+    }
+}
---
+++ AdventOfCode23Test/TestDay02.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode23Day;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace AdventOfCode23Test;
+
+
+[TestClass]
+public class TestDay02
+{
+    private readonly Day02 _day02 = new();
+
+    [TestInitialize]
+    public void TestInit()
+    {
+
+    }
+
+    [TestMethod]
+    [DataRow("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        4, 2, 6)]
+    [DataRow("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        1, 3, 4)]
+    [DataRow("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        20, 13, 6)]
+    [DataRow("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        14, 3, 15)]
+    [DataRow("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        6, 3, 2)]
+    public void TestDay02_DeductMinimumCubeFromGameString
+        (string gameString, int expectedRed, int expectedGreen, int expectedBlue)
+    {
+        var actual = _day02.DeductMinimumCubes(gameString);
+
+        actual["blue"].Should().Be(expectedBlue);
+        actual["red"].Should().Be(expectedRed);
+        actual["green"].Should().Be(expectedGreen);
+    }
+}
